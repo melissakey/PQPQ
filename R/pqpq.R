2811a151686c87dcda37cc2ac7817c37bc886f4f
Melissa Key <melkey@iu.edu>
2018-07-22 19:40:28 -0400

    Added wrapper around the individual functions to streamline the PQPQ process.

---
+++ R/pqpq.R
@@ -0,0 +1,61 @@
+#' PQPQ
+#'
+#' @description This function applies the PQPQ algorithm to proteomic data to filter out poorly correlating peptides and cluster the remaining peptides into likely proteoforms.
+#' @param df The data frame with proteomic data.
+#' @param sample_names A character vector identifying the columns holding the quantitative peptide data.  Optional IF (1) the data-type is not "manually annotated" and all columns are desired.
+#' @param protein_subset The subset of proteins on which to apply the filter.
+#' @param data_type One of "Protein Pilot", "Spectrum Mill", "Proteome Discoverer", or "Manually annotated" - Used to determine the columns containing different data elements.
+#' @param normalize_data Should the data be normalized?  Default is TRUE for compatibility with the Matlab version.
+#' @param correlation_p_value Correlations with p-values below this threshold are determined to be significant.
+#' @param high_confidence_limit Minimum confidence for a peptide to be considered highly likley to be identified correctly.
+#' @param peptide_sum_intensity_limit Minimum intensity for peptide to be included.
+#' @param separate_multiple_protein_IDs If TRUE, peptides assigned to multiple proteins are copied and analyzed multiple times as part of each protein to which it is assigned.  If FALSE, peptides are assigned to the protein group, and analyzed once.  Default is FALSE.
+#' @param manually_annotated_fields If data_type is "Manually annotated", this is a list of the columns needed to complete the filtering: protein_id, confidence, and peptide_ids.  See examples
+#' @details
+#' This function performs the PQPQ process - including preprocessing, peptide_selection, and filtering.
+#' @export
+#' @return A data frame identifying which peptides are kept, and which proteoforms they are assigned to.
+#' @examples
+#' data("testdata2")
+#'
+#' data("testdata2")
+#' sample_names <- stringr::str_subset(names(testdata2), "^Area")[-7]
+#' column_ids <- list(
+#'   protein_id = "Accessions",
+#'   confidence = 'Conf',
+#'   peptide_ids = "Sequence"
+#' )
+#'
+#' result <- pqpq(testdata2, sample_names = sample_names, data_type = "Manually annotated", manually_annotated_fields = column_ids)
+
+
+pqpq <- function(df,                    # data frame with input data
+  sample_names = NULL,
+  protein_subset = NULL,
+  data_type = c("Protein Pilot", "Spectrum Mill", "Proteome Discoverer", "Manually annotated"),
+  normalize_data = TRUE,
+  correlation_p_value = 0.4,
+  high_confidence_limit = 95,
+  peptide_sum_intensity_limit = 0,
+  separate_multiple_protein_IDs = FALSE,
+  manually_annotated_fields = NULL,
+  action = c('mark', 'filter')
+) {
+
+  action <- match.arg(action)
+  data_type <- match.arg(data_type)
+
+  processed_data <- preprocess_pqpq_input(df,
+    data_type = data_type,
+    protein_subset = protein_subset,
+    separate_multiple_protein_IDs = separate_multiple_protein_IDs,
+    sample_names = sample_names,
+    manually_annotated_fields
+  )
+
+  data_list <- with(processed_data, peptide_selection(data,
+    column_ids, the_limit = peptide_sum_intensity_limit, p_val = correlation_p_value, peptide_confidence_limit = high_confidence_limit, do_normalization = normalize_data))
+
+  output <- filter_peptides(data_list, processed_data$column_ids, action = action)
+}
+
--- R/preprocess_pqpq_data.R
+++ R/preprocess_pqpq_data.R
@@ -6,18 +6,10 @@
 #' @param data_type one of 'Protein Pilot', 'Spectrum Mill', or 'Proteome Discoverer', depending on the file origin.
 #' @param protein_subset identifiers for the proteins to be analyzed.
 #' @param separate_multiple_protein_IDs If true, peptides assigned to multiple proteins are duplicated for each protein to which they are assigned.  Otherwise, they are analyzed as a group.
-#' @param column_ids A list of variables identifiers.
+#' @param sample_names A character vector identifying the columns holding sample data.
+#' @param manually_annotated_fields A list containing annotation data.
 #'
 #' @details
-#' This function handles most of the pre-processing done in the PQPQ function including:
-#' \itemize{
-#' \item{If \code{column_ids} is NULL, creates a list of which column names identifying the relevant portions of the data set (confidence, protein ID, peptide ID, and samples).
-#' This is subject to error depending on how the data was input into R and the data type.}
-#' \item{confirms that the specified columns are in the data set.}
-#' \item{removes peptides which do not meet PQPQ criterion. (in Protein Pilot, this includes proteins with annotation 'auto - shared MS/MS')}
-#' \item{if separate_multiple_proteins_IDs = TRUE, assigns peptides belonging to multiple proteins to ALL listed proteins separately, not the protein group.}
-#' \item{if a protein subset is given, filters out all other proteins.}
-#'}
 #' Note: this is only tested on the test output from Protein Pilot provided by Jenny Forshed in the PQPQ package, and entered into R using the \code{link{openxlsx}} package.  The formatting of data from other
 #'
 #' @export
@@ -40,45 +32,61 @@ preprocess_pqpq_input <- function(df,
   data_type = "Protein Pilot",
   protein_subset = NULL,
   separate_multiple_protein_IDs = FALSE,
-  column_ids = NULL
+  sample_names = NULL,
+  manually_annotated_fields = NULL
 ) {
 
   df <- within(df,{
-    peptide_id <- 1:nrow(df)
+    if(!exists("peptide_id",inherits = FALSE)) {
+      peptide_id <- 1:nrow(df)
+    } else if(length(unique(peptide_id)) != length(peptide_id)) {
+      peptide_id <- paste(peptide_id, 1:nrow(df), sep = "_")
+    }
   })
 
-  if (is.null(column_ids)) {
-    column_ids <- switch(data_type,
-      `Protein Pilot` = {
-        list(protein_id = "Accessions",
-          protein_name = "Names",
-          confidence = 'Conf',
-          sample_names = grep("Area",names(df),value = TRUE),
-          peptide_ids = c("Sequence","peptide_id")
-        )
-      },
-      `Spectrum Mill` = {
-        warning("check these results - Melissa was not able to test this code.")
-        list(
-          protein_id = 'accessionnumber',
-          protein_name = 'entryname',
-          confidence = "score",
-          sample_names = grep("iTRAQ_1",names(df),value = TRUE),
-          peptide_ids = c("sequence","peptide_id")
-        )
-      },
-      `Proteome Discoverer` = {
-        warning("check these results - Melissa was not able to test this code.")
-        list(
-          protein_id = c('ProteinAccessions'),
-          confidence = {if(exists("df$IonScore")) "IonScore" else "XCorr"},
-          sample_names = grep("^11X",names(df),value = TRUE),
-          peptide_ids = c("sequence","peptide_id")
-        )
-      },
-      stop("Please give a valid data_type or manually list column names.")
-    )
-  }
+  column_ids <- switch(data_type,
+    `Protein Pilot` = {
+      list(protein_id = "Accessions",
+        protein_name = "Names",
+        confidence = 'Conf',
+        sample_names = {if(!is.null(sample_names)) sample_names else grep("Area",names(df), value = TRUE)},
+        peptide_ids = c("Sequence","peptide_id")
+      )
+    },
+    `Spectrum Mill` = {
+      warning("check these results - Melissa was not able to test this code.")
+      list(
+        protein_id = 'accessionnumber',
+        protein_name = 'entryname',
+        confidence = "score",
+        sample_names = {if(!is.null(sample_names)) sample_names else grep("iTRAQ_1",names(df),value = TRUE)},
+        peptide_ids = c("sequence","peptide_id")
+      )
+    },
+    `Proteome Discoverer` = {
+      warning("check these results - Melissa was not able to test this code.")
+      list(
+        protein_id = c('ProteinAccessions'),
+        confidence = {if(exists("df$IonScore")) "IonScore" else "XCorr"},
+        sample_names = {if(!is.null(sample_names)) sample_names else grep("^11X",names(df),value = TRUE)},
+        peptide_ids = c("sequence","peptide_id")
+      )
+    },
+    `Manually annotated` = {
+      if(is.null(manually_annotated_fields)) stop("Manual annotation requires `manually_annotated_fields` to be defined")
+
+      manually_annotated_fields$sample_names = sample_names
+      if(
+        !is.list(manually_annotated_fields) |
+          length(setdiff(c("protein_id", "confidence", "sample_names", "peptide_ids"), names(manually_annotated_fields) )) > 0)
+        stop("Manual annotation requires `manually_annotated fields to be a list with elements `protein_id`, `confidence`, and `peptide_ids`; and `sample_names` must be defined.")
+
+      manually_annotated_fields <- within(manually_annotated_fields, {
+        peptide_ids <- unique(peptide_ids, "peptide_id")
+      })
+    },
+    stop("Please give a valid data_type or manually list column names.")
+  )
   # check data parameters
   column_ids <- check_column_names(df, column_ids)
 
@@ -89,7 +97,6 @@ preprocess_pqpq_input <- function(df,
     warning("check to make sure correct proteins are used.")
     df <- subset(df, QuanUsage == 'Used' & ProteinGroups != 0)
   }
-
 
   if(separate_multiple_protein_IDs) {
     df <- plyr::ddply(df, column_ids$protein_id, function(protein_df) {
@@ -111,7 +118,7 @@ preprocess_pqpq_input <- function(df,
 
   if(!is.null(protein_subset)){
     list(
-      parameters = column_ids,
+      column_ids = column_ids,
       data = df[df[[column_ids$protein_id]] %in% protein_subset, ]
     )
 
