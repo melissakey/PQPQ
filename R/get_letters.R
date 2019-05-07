a7076917bbc46bd09fb35074c50716171a63ce6e
Melissa Key <melkey@iu.edu>
2019-05-07 09:28:40 -0400

    fixed bug in code that prevented get.letters(1) from working.

--- R/get_letters.R
+++ R/get_letters.R
@@ -1,6 +1,6 @@
 make.chars <- function(length.out, case, n.char = NULL) {
   if(is.null(n.char))
-    n.char <- ceiling(log(length.out, 26))
+    n.char <- max(1, ceiling(log(length.out, 26)))
   m <- sapply(n.char:1, function(x) {
     rep(rep(1:26, each = 26^(x-1)) , length.out = length.out)
   })
@@ -9,14 +9,15 @@ make.chars <- function(length.out, case, n.char = NULL) {
       'upper' = LETTERS[m]
     )
     m.char <- LETTERS[m]
-    dim(m.char) <- dim(m)
+    if(is.null(dim(m))) dim(m.char) <- c(1,n.char)
+    else dim(m.char) <- dim(m)
 
     apply(m.char, 1, function(x) paste(x, collapse = ""))
 }
 
 
 get.letters <- function(length.out, case = 'upper'){
-  max.char <- ceiling(log(length.out, 26))
+  max.char <- max(1, ceiling(log(length.out, 26)))
   grp <- rep(1:max.char, 26^(1:max.char))[1:length.out]
   unlist(lapply(unique(grp), function(n) make.chars(length(grp[grp == n]), case = case, n.char = n)))
 }
